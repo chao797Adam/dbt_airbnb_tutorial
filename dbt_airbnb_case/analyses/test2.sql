ef5940402334b312095655dcabe1624397ac4bac
xc797 <chao.xu268@gmail.com>
2026-06-13 00:20:54 +1000

    Amend test files

--- dbt_airbnb_case/analyses/test2.sql
+++ dbt_airbnb_case/analyses/test2.sql
@@ -1,1 +1,5 @@
-select * from {{ source('airbnb_source', 'bookings') }} limit 5
+{% set nights_booked_threshold = 1 %}
+select *
+from {{ ref('stg_bookings') }}
+where nights_booked > {{ nights_booked_threshold }}
+order by nights_booked
