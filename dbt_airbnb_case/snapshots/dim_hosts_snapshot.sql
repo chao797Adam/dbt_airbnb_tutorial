c8070bad9af84e195c091459f333173b6e256bf8
xc797 <chao.xu268@gmail.com>
2026-06-14 21:57:59 +1000

    Amend snapshot

--- dbt_airbnb_case/snapshots/dim_hosts_snapshot.sql
+++ dbt_airbnb_case/snapshots/dim_hosts_snapshot.sql
@@ -1,15 +1,19 @@
 {% snapshot hosts_snapshot %}
 
-{{
+    {{
     config(
       target_schema='snapshots',
       unique_key='host_id',
-      strategy='timestamp',
-      updated_at='ingested_at',
-      dbt_valid_to_current = "to_date('9999-12-31')"
+      strategy='check',
+      check_cols=[
+          'host_name',
+          'is_superhost',
+          'response_rate'
+      ]
     )
 }}
 
-select * from {{ ref('stg_hosts') }}
+    select *
+    from {{ ref('stg_hosts') }}
 
 {% endsnapshot %}
--- dbt_airbnb_case/snapshots/dim_listings_snapshot.sql
+++ dbt_airbnb_case/snapshots/dim_listings_snapshot.sql
@@ -1,15 +1,15 @@
 {% snapshot listings_snapshot %}
 
-{{
+    {{
     config(
       target_schema='snapshots',
-      unique_key='listing_id',     
-      strategy='timestamp',
-      updated_at='ingested_at',    
-      dbt_valid_to_current="to_date('9999-12-31')"
+      unique_key='listing_id',
+      strategy='check',
+      check_cols=['price_per_night', 'room_type', 'bedrooms']
     )
 }}
 
-select * from {{ ref('stg_listings') }}
+    select *
+    from {{ ref('stg_listings') }}
 
 {% endsnapshot %}
